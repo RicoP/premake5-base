bc52c6c91ad0f7ec074e2f933bd9847e56194bb6
RicoP <RicoP@users.noreply.github.com>
2020-03-16 16:59:59 +0100

    add sokol

--- externals/clone.bat
+++ externals/clone.bat
@@ -1,5 +1,6 @@
-git clone --depth 1 --recurse-submodules -j8 git@github.com:nothings/stb.git include/stb
-git clone --depth 1 --recurse-submodules -j8 git@github.com:RandyGaul/cute_headers.git include/cute_headers
+git clone --depth 1 git@github.com:nothings/stb.git include/stb
+git clone --depth 1 git@github.com:RandyGaul/cute_headers.git include/cute_headers
+git clone --depth 1 git@github.com:floooh/sokol.git include/sokol
 git clone --depth 1 git@github.com:electronicarts/EAAssert.git
 git clone --depth 1 git@github.com:electronicarts/EABase.git
 git clone --depth 1 git@github.com:electronicarts/EAStdC.git
